09054653695e387c2cbb97705a1fe9eb94138e24
Rustambek <rusik.khanov06@gmail.com>
2026-03-02 19:56:55 +0500

    BMP280 Sensor

--- Projects/IoT-Erasmus/src/main.cpp
+++ Projects/IoT-Erasmus/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include <Adafruit_BMP280.h>
 
+Adafruit_BMP280 bmp;
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  if (!bmp.begin(0x76)) {
+    Serial.println("Could not find BMP");
+    while(1);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.println("");
+
+  delay(2000);
 }
